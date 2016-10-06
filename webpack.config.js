73c3527fe29d00ffd14de881d6f683d87c932a0b
Dariusz Pobożniak <dariusz.pobozniak@performgroup.com>
2016-10-06 22:16:11 +0200

    Add webpack build option

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
 var path = require('path');
 var webpack = require('webpack');
+var env = process.env.NODE_ENV;
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
+  // devtool: 'cheap-module-eval-source-map',
+  devtool: env === 'production' ? 'cheap-module-source-map' : 'eval-source-map',
   context: path.join(__dirname, './src'),
   entry: {
-    js: './app.js',
-    vendor: ['react', 'react-dom']
+    js: './autosuggest/Autosuggest.js',
+    vendor: ['react', 'react-dom'],
   },
   output: {
     path: path.join(__dirname, './static'),
     filename: 'bundle.js',
     // publicPath: '/dist/'
   },
-    /*plugins: [
-        new webpack.NoErrorsPlugin()
-        ],*/
+
   module: {
     loaders: [
       {
         test: /\.html$/,
         loader: 'file',
         query: {
-          name: '[name].[ext]'
-        }
+          name: '[name].[ext]',
+        },
       },
       {
         test: /\.js$/,
@@ -34,25 +34,30 @@ module.exports = {
         test: /\.css$/,
         loaders: [
           'style',
-          'css'
-        ]
+          'css',
+        ],
       },
-    ]
+    ],
   },
 
   resolve: {
     extensions: ['.js'],
     modules: [
       path.resolve('./src'),
-      'node_modules'
-    ]
+      'node_modules',
+    ],
   },
 
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
       minChunks: Infinity,
-      filename: 'vendor.bundle.js'
+      filename: 'vendor.bundle.js',
     }),
-  ]
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(env),
+      },
+    }),
+  ],
 };
